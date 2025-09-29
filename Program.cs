19a475ddc563f6cf507e5d26a4d4f06e1830946c
unknown <aaden.donaldson@students.snow.edu>
2025-09-29 12:03:53 -0600

    Final Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+Console.Clear();
+Console.WriteLine("-Welcome to the game of sticks!!!!-");
+Console.WriteLine("*Cheering from the crowd*");
+
+Console.WriteLine("Players will take turns removing 1 to 3 of the remaining sticks. The player that removes the last stick loses.");
+int sticksTotal = 20;
+int maxSticks = 3;
+int currentPlayer = 1;
+while (sticksTotal > 0) 
+{
+    Console.Clear();
+
+    Console.WriteLine("Sticks Left: " + sticksTotal); 
+
+    int sticksTaken;
+    while (true)
+    {
+         Console.Write("Player " + currentPlayer + ", how many sticks would you like to take?(Type answer on next line)");
+        Console.ReadLine();
+
+        string? input = Console.ReadLine();
+
+        if (int.TryParse(input, out sticksTaken))
+        {
+            if (sticksTaken >= 1 && sticksTaken <= maxSticks && sticksTaken <= sticksTotal)
+            {
+                break;
+
+            }
+            else
+            {
+                Console.WriteLine($"Invalid! Please try again with a number that will actually work!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Put a number in!");
+        }
+    }
+    sticksTotal -= sticksTaken;
+
+    if (sticksTotal <= 0)
+    {
+        Console.WriteLine($"Player " + currentPlayer + " took the last stick! Hahaha, he is a loser face!");
+        int winner = (currentPlayer == 1) ? 2 : 1;
+        Console.WriteLine($"Player " + winner + " wins! What a good boy!");
+    }
+    else
+    {
+        currentPlayer = (currentPlayer == 1) ? 2 : 1;
+    }
+}
+Console.WriteLine("What a game!!! Friendships destroyed and now we have procrastinated our homework even further! Well I guess goodbye for now");
--- obj/Debug/net8.0/lab-4-game-of-sticks-aadendonaldson.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-4-game-of-sticks-aadendonaldson.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-4-game-of-sticks-aadendonaldson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+698ce7c3af68e46fd7a97d2e50d07aaab0fa1770")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3794f309eab2baedb10798f760e678207a504a69")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-4-game-of-sticks-aadendonaldson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-4-game-of-sticks-aadendonaldson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
